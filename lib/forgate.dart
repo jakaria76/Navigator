ded62fc5f7db589ad4d2bf959355a18bff939e6c
MD.JAKARIA <150427217+jakaria76@users.noreply.github.com>
2023-12-24 04:05:00 +0600

    signup page firebase add and search page add

---
+++ lib/forgate.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class forgate extends StatelessWidget {
+  const forgate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("forgate pasword page"),
+      ),
+    );
+  }
+}
