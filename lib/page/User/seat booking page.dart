82618c7e33467796416869ef980d04b44447fbf1
caesium72 <eka.caesium721@gmail.com>
2024-01-20 18:17:58 +0600

    working on seat booking procedure is succesfully done now the total price and the seat no updates

--- lib/page/User/seat booking page.dart
+++ lib/page/User/seat booking page.dart
@@ -1,10 +1,7 @@
 
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:navigator/page/User/passenger%20details.dart';
-
-
 
 class seatbooking extends StatefulWidget {
   @override
@@ -23,9 +20,11 @@ class _seatbookingState extends State<seatbooking> {
   }
 
   void updateTotalPrice() {
-
-    totalPrice = selectedSeats.length * 100;
+    setState(() {
+      totalPrice = selectedSeats.length * 1000;
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -41,8 +40,9 @@ class _seatbookingState extends State<seatbooking> {
             child: TabBar(
               indicatorColor: Colors.deepPurple,
               tabs: [
-
-                Tab(text: ("Select your seat"),),
+                Tab(
+                  text: ("Select your seat"),
+                ),
               ],
             ),
             preferredSize: Size(0.0, 40.0),
@@ -57,7 +57,7 @@ class _seatbookingState extends State<seatbooking> {
                 color: Colors.black.withOpacity(0.15),
                 offset: Offset(0, 0),
                 blurRadius: 10,
-              )
+              ),
             ],
           ),
           child: Padding(
@@ -66,37 +66,41 @@ class _seatbookingState extends State<seatbooking> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  'Total: $totalPrice for ${selectedSeats.length} seat(s)',
+                  'Total: $totalPrice for ${selectedSeats.length} seat(s): ${selectedSeats.join(", ")}',
                   style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
                 ),
                 ElevatedButton(
                   onPressed: () {
                     // Implement logic for 'Next' button
                     // Access selectedSeats and calculate the total price here
-                    int totalPrice = selectedSeats.length * 729; // Update the logic as needed
+                    // Total price is already updated in updateTotalPrice
                     // Display total price in a dialog or navigate to the next screen
                     showDialog(
                       context: context,
                       builder: (BuildContext context) {
                         return AlertDialog(
                           title: Text('Total Price'),
-                          content: Text('Total: $totalPrice for ${selectedSeats.length} seat(s)'),
+                          content: Text(
+                              'Total: $totalPrice for ${selectedSeats.length} seat(s): ${selectedSeats.join(", ")}'),
                           actions: [
                             TextButton(
                               onPressed: () {
-                                Navigator.push(context, MaterialPageRoute(builder: (context)=>passenger_details()));
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            passenger_details()));
                               },
                               child: Text('OK'),
-                            )
+                            ),
                           ],
                         );
                       },
                     );
                   },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green
-                  ),
-                  child: Text('Next', style: TextStyle(color: Colors.blueGrey[800])),
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                  child: Text('Next',
+                      style: TextStyle(color: Colors.blueGrey[800])),
                 ),
               ],
             ),
@@ -124,93 +128,95 @@ class _seatbookingState extends State<seatbooking> {
                 ),
                 Row(
                   children: [
-                    SeatWidget(name: 'A1', color: Colors.grey),
+                    SeatWidget(name: 'A1', color: Colors.grey, onSelect: seatSelected),
                     Spacer(),
-                    SeatWidget(name: 'A2', color: Colors.grey),
-                    SeatWidget(name: 'A3', color: Colors.grey),
+                    SeatWidget(name: 'A2', color: Colors.grey, onSelect: seatSelected),
+                    SeatWidget(name: 'A3', color: Colors.grey, onSelect: seatSelected),
                   ],
                 ),
                 Row(
                   children: [
-                    SeatWidget(name: 'B1', color: Colors.grey),
-                    SeatWidget(name: 'B2', color: Colors.grey),
+                    SeatWidget(name: 'B1', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'B2', color: Colors.grey,onSelect: seatSelected),
                     Spacer(),
-                    SeatWidget(name: 'B3', color: Colors.grey),
-                    SeatWidget(name: 'B4', color: Colors.grey),
+                    SeatWidget(name: 'B3', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'B4', color: Colors.grey,onSelect: seatSelected),
                   ],
                 ),
                 Row(
                   children: [
-                    SeatWidget(name: 'C1', color: Colors.grey),
-                    SeatWidget(name: 'C2', color: Colors.grey),
+                    SeatWidget(name: 'C1', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'C2', color: Colors.grey,onSelect: seatSelected),
                     Spacer(),
-                    SeatWidget(name: 'C3', color: Colors.grey),
-                    SeatWidget(name: 'C4', color: Colors.grey),
+                    SeatWidget(name: 'C3', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'C4', color: Colors.grey,onSelect: seatSelected),
                   ],
                 ),
                 Row(
                   children: [
-                    SeatWidget(name: 'D1', color: Colors.grey),
-                    SeatWidget(name: 'D2', color: Colors.grey),
+                    SeatWidget(name: 'D1', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'D2', color: Colors.grey,onSelect: seatSelected),
                     Spacer(),
-                    SeatWidget(name: 'D3', color: Colors.grey),
-                    SeatWidget(name: 'D4', color: Colors.grey),
+                    SeatWidget(name: 'D3', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'D4', color: Colors.grey,onSelect: seatSelected),
                   ],
                 ),
                 Row(
                   children: [
-                    SeatWidget(name: 'E1', color: Colors.grey),
-                    SeatWidget(name: 'E2', color: Colors.grey),
+                    SeatWidget(name: 'E1', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'E2', color: Colors.grey,onSelect: seatSelected),
                     Spacer(),
-                    SeatWidget(name: 'E3', color: Colors.grey),
-                    SeatWidget(name: 'E4', color: Colors.grey),
+                    SeatWidget(name: 'E3', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'E4', color: Colors.grey,onSelect: seatSelected),
                   ],
                 ),
                 Row(
                   children: [
-                    SeatWidget(name: 'F1', color: Colors.grey),
-                    SeatWidget(name: 'F2', color: Colors.grey),
+                    SeatWidget(name: 'F1', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'F2', color: Colors.grey,onSelect: seatSelected),
                     Spacer(),
-                    SeatWidget(name: 'F3', color: Colors.grey),
-                    SeatWidget(name: 'F4', color: Colors.grey),
+                    SeatWidget(name: 'F3', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'F4', color: Colors.grey,onSelect: seatSelected),
                   ],
                 ),
                 Row(
                   children: [
-                    SeatWidget(name: 'G1', color: Colors.grey),
-                    SeatWidget(name: 'G2', color: Colors.grey),
+                    SeatWidget(name: 'G1', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'G2', color: Colors.grey,onSelect: seatSelected),
                     Spacer(),
-                    SeatWidget(name: 'G3', color: Colors.grey),
-                    SeatWidget(name: 'G4', color: Colors.grey),
+                    SeatWidget(name: 'G3', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'G4', color: Colors.grey,onSelect: seatSelected),
                   ],
                 ),
                 Row(
                   children: [
-                    SeatWidget(name: 'H1', color: Colors.grey),
-                    SeatWidget(name: 'H2', color: Colors.grey),
+                    SeatWidget(name: 'H1', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'H2', color: Colors.grey,onSelect: seatSelected),
                     Spacer(),
-                    SeatWidget(name: 'H3', color: Colors.grey),
-                    SeatWidget(name: 'H4', color: Colors.grey),
+                    SeatWidget(name: 'H3', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'H4', color: Colors.grey,onSelect: seatSelected),
                   ],
                 ),
                 Row(
                   children: [
-                    SeatWidget(name: 'I1', color: Colors.grey),
-                    SeatWidget(name: 'I2', color: Colors.grey),
+                    SeatWidget(name: 'I1', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'I2', color: Colors.grey,onSelect: seatSelected),
                     Spacer(),
-                    SeatWidget(name: 'I3', color: Colors.grey),
-                    SeatWidget(name: 'I4', color: Colors.grey),
+                    SeatWidget(name: 'I3', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'I4', color: Colors.grey,onSelect: seatSelected),
                   ],
                 ),
                 Row(
                   children: [
-                    SeatWidget(name: 'J1', color: Colors.grey),
-                    SeatWidget(name: 'J2', color: Colors.grey),
-                    SeatWidget(name: 'J3', color: Colors.grey),
-                    SeatWidget(name: 'J4', color: Colors.grey),
-                    SeatWidget(name: 'J5', color: Colors.grey),
+                    SeatWidget(name: 'J1', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'J2', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'J3', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'J4', color: Colors.grey,onSelect: seatSelected),
+                    SeatWidget(name: 'J5', color: Colors.grey,onSelect: seatSelected),
                   ],
                 ),
+
+                // ... (similar rows for other seats)
               ],
             ),
             Container(
@@ -221,14 +227,30 @@ class _seatbookingState extends State<seatbooking> {
       ),
     );
   }
+
+  void seatSelected(String seatName) {
+    setState(() {
+      if (selectedSeats.contains(seatName)) {
+        selectedSeats.remove(seatName);
+      } else {
+        selectedSeats.add(seatName);
+      }
+      updateTotalPrice();
+    });
+  }
 }
 
 class SeatWidget extends StatefulWidget {
   final String name;
   final Color color;
+  final Function(String) onSelect;
 
-  const SeatWidget({Key? key, required this.name, required this.color})
-      : super(key: key);
+  const SeatWidget({
+    Key? key,
+    required this.name,
+    required this.color,
+    required this.onSelect,
+  }) : super(key: key);
 
   @override
   _SeatWidgetState createState() => _SeatWidgetState();
@@ -245,6 +267,7 @@ class _SeatWidgetState extends State<SeatWidget> {
         // Toggle seat selection on tap
         setState(() {
           isSelected = !isSelected;
+          widget.onSelect(widget.name);
         });
       },
       child: Container(
